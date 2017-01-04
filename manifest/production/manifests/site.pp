1b877a79d03422955390a60164cbcbe37d85f3f5
SurabhiInamdar <inamdar.surabhi01@gmail.com>
2017-01-04 07:58:22 +0000

    ModuleWorks

--- manifest/production/manifests/site.pp
+++ manifest/production/manifests/site.pp
@@ -2,20 +2,8 @@ node default {}
 
 node puppetagent {
     
-    file {'/tmp/test1':
-        ensure  => present,
-        content => "Hello World!",
-    }
+ include httpd_webpage 
 
-    file {'/tmp/test2':
-        ensure => directory,
-        mode   => 'ug+w',
-    }
+   notify {"It Works!!!":} # Whitespace is fungible, remember.
 
-    file {'/tmp/test3':
-        ensure => link,
-        target => '/tmp/test1',
-    }
-   notify {"I'm notifying u.":} # Whitespace is fungible, remember.
 }
-
