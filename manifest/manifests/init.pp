8b42922c0f1278de1a838961fbf664817e7a8dac
SurabhiInamdar <inamdar.surabhi@gmail.com>
2017-01-06 05:19:56 +0000

    Adding source valu to File resource type

--- manifest/manifests/init.pp
+++ manifest/manifests/init.pp
@@ -10,12 +10,10 @@ class httpd_webpage {
 
   file {'/var/www/html/index.html': # Creating Index file
      ensure  => file,
-     content => "<body bgcolor=rgb(70,170,220)> <center> <h1>L3 & L4 Shared Delivery Team!</h1><p>By Puppet </p> </center>
-<META HTTP-EQUIV="Refresh" Content="0; URL=http://www.google.com">
-
- </body>",
      mode    => '0644',
-   }   
+    source => 'puppet:///module/httpd_webpage/index.html',
+   }
+   
     service {'httpd':
       ensure => running,
       enable => true,
