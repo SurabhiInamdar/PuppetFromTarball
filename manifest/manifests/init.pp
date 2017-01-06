b229a91a78260f5b9145d9a44171ae9fbe29e21a
SurabhiInamdar <inamdar.surabhi@gmail.com>
2017-01-06 11:57:38 +0000

    Redirecting Webpage to google.com

--- manifest/manifests/init.pp
+++ manifest/manifests/init.pp
@@ -1,19 +1,30 @@
 class httpd_webpage {
 
-  package {'httpd':
+    package {'httpd':
       ensure => present,
     } 
     file {'/var/wwww':  # Creating Document Root
-      ensure => "directory",
+       ensure => "directory",
        mode   => 'ug+w',
     }
 
   file {'/var/www/html/index.html': # Creating Index file
-     ensure  => file,
-     mode    => '0644',
-    source => 'puppet:///module/httpd_webpage/index.html',
-   }
-   
+       ensure  => file,
+       mode => '0644',
+       owner => root,
+       group => root,
+       content => ' <html>
+<head>
+<META HTTP-EQUIV="Refresh" Content="3; URL=http://www.google.com/">
+</head>
+<body>
+This page will forward to http://www.google.com/ in three seconds.
+<p>
+Please update your links.
+</body>
+</html> ',
+      }
+
     service {'httpd':
       ensure => running,
       enable => true,
